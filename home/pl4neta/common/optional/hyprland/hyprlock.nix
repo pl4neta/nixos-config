7bca4c5a0659457a6c041994b59e6135ecfc09bd
pl4neta <135662804+pl4neta@users.noreply.github.com>
2025-10-15 01:11:21 +0100

    hyprlock.nix

--- home/pl4neta/common/optional/hyprland/hyprlock.nix
+++ home/pl4neta/common/optional/hyprland/hyprlock.nix
@@ -1,100 +1,99 @@
-{ pkgs, lib, ... }:
-let 
-  text = "rgb(251, 241, 199)";
-in
+{ lib, ... }:
 {
-  home.packages = [ pkgs.hyprlock ];
-  xdg.configFile."hypr/hyprlock.conf".text = ''
-    # BACKGROUND
-    background {
-      monitor =
-      path = ${lib.custom.relativeToRoot "assets/wallpapers/perfect-blue.jpeg"}
-      blur_passes = 1
-      contrast = 0.8916
-      brightness = 0.8172
-      vibrancy = 0.1696
-      vibrancy_darkness = 0.0
-    }
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+	    "$font" = "JetBrainsMono Nerd Font";
+      "$font2" = "Montserrat";
+      "$text" = "rgb(198, 208, 245)";
+      "$accent" = "rgba(186, 187, 241, .5)";
+      "$base" = "rgba(165, 173, 206, .2)";
+      "$warn" = "rgba(229, 200, 144, .5)";
+      "$err" = "rgba(231, 130, 132, .5)";
 
-    # GENERAL
-    general {
-      hide_cursor = true
-      no_fade_in = false
-      grace = 0
-      disable_loading_bar = false
-    }
+# GENERAL
+	    "general" = {
+		    "hide_cursor" = true;
+	    };
 
-    # Time
-    label {
-      monitor = 
-      text = cmd[update:1000] echo "$(date +"%k:%M")"
-      color = rgba(235, 219, 178, .9)
-      font_size = 111
-      font_family = JetBrainsMono NF Bold
-      position = 0, 270
-      halign = center
-      valign = center
-    }
+# BACKGROUND
+	    "background" = {
+		    "monitor" = "";
+		    "path" = "${lib.custom.relativeToRoot "assets/wallpapers/perfect-blue.jpeg"}";
+		    "blur_passes" = 3;
+        "blur_size" = 1;
+	    };
 
-    # Day
-    label {
-      monitor =
-      text = cmd[update:1000] echo "- $(date +"%A, %B %d") -"
-      color = rgba(235, 219, 178, .9)
-      font_size = 20
-      font_family = CaskaydiaCove Nerd Font
-      position = 0, 160
-      halign = center
-      valign = center
-    }
+	    label = [
+# TIME
+	    {
+		    "monitor" = "";
+		    "text" = "$TIME";
+		    "color" = "$text";
+		    "font_size" = 130;
+		    "font_family" = "$font";
+		    "position" = "0, 155";
+		    "halign" = "center";
+		    "valign" = "center";
+	    }
+# DATE
+	    {
+		    "monitor" = "";
+		    "text" = ''cmd[update:1000] echo "- $(date +"%A, %B %d") -"'';
+		    "color" = "$text";
+		    "font_size" = 30;
+		    "font_family" = "$font2";
+		    "position" = "0, 45";
+		    "halign" = "center";
+		    "valign" = "center";
+	    }
+# USER
+      {
+        "monitor" = "";
+        "text" = "  $USER";
+        "color" = "$text";
+        "font_size" = "16";
+        "font_family" = "$font2";
+        "position" = "0, -155";
+        "halign" = "center";
+        "valign" = "center";
+      }
+	    ];
 
+# USER-BOX
+    "shape" = {
+      "monitor" = "";
+      "size" = "350, 50";
+      "color" = "$base";
+      "rounding" = "15";
+      "border_size" = "0";
+      "rotate" = "0";
+      "position" = "0, -155";
+      "halign" = "center";
+      "valign" = "center";
+    };
 
-    # USER-BOX
-    shape {
-      monitor =
-      size = 350, 50
-      color = rgba(225, 225, 225, .2)
-      rounding = 15
-      border_size = 0
-      border_color = rgba(255, 255, 255, 0)
-      rotate = 0
-
-      position = 0, -230
-      halign = center
-      valign = center
-    }
-
-    # USER
-    label {
-      monitor =
-      text =   $USER
-      color = rgba(235, 219, 178, .9)
-      font_size = 16
-      font_family = CaskaydiaCove Nerd Font
-      position = 0, -230
-      halign = center
-      valign = center
-    }
-
-    # INPUT FIELD
-    input-field {
-      monitor =
-      size = 350, 50
-      outline_thickness = 0
-      rounding = 15
-      dots_size = 0.25 # Scale of input-field height, 0.2 - 0.8
-      dots_spacing = 0.4 # Scale of dots' absolute size, 0.0 - 1.0
-      dots_center = true
-      outer_color = rgba(255, 255, 255, 0)
-      inner_color = rgba(225, 225, 225, 0.2)
-      color = rgba(235, 219, 178, .9)
-      font_color = rgba(235, 219, 178, .9)
-      fade_on_empty = false
-      placeholder_text = <i><span foreground="##ebdbb2e5">Enter Password</span></i>
-      hide_input = false
-      position = 0, -300
-      halign = center
-      valign = center
-    }
-  '';
+    "input-field" = {
+      "monitor" = "";
+      "size" = "350, 50";
+      "outline_thickness" = "0";
+      "rounding" = "15";
+      "dots_size" = "0.25";
+      "dots_spacing" = "0.4";
+      "dots_center" = "true";
+      "inner_color" = "$base";
+      "color" = "$text";
+      "font_color" = "$text";
+      "fade_on_empty" = "false";
+		  "check_color" = "$accent";
+		  "capslock_color" = "$warn";
+		  "fail_color" = "$err";
+      "placeholder_text" = ''<i><span foreground="##ebdbb2e5">Enter Password</span></i>'';
+      "hide_input" = "false";
+      "position" = "0, -225";
+      "halign" = "center";
+      "valign" = "center";
+    };
+    };
+  };
 }
